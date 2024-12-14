e64b2f2186b77f9f5de38c63e81529e63e4aa10c
gitlab.com/pepa65 <pepa65@passchier.net>
2024-12-14 22:56:05 +0700

    Mods

--- sound.js
+++ sound.js
@@ -1,17 +1,16 @@
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+const audioCtx=new (window.AudioContext || window.webkitAudioContext)();
+const defDuration=2;
+const gainStep=0.05
 
-function playSound(frequency = 440, duration = 2) {
-   const osc = audioCtx.createOscillator();
-   const envelope = audioCtx.createGain();
-   osc.connect(envelope);
-   envelope.connect(audioCtx.destination);
-
-   envelope.gain.setValueAtTime(0, audioCtx.currentTime);
-   envelope.gain.linearRampToValueAtTime(0.05, audioCtx.currentTime + 0.05);
-   envelope.gain.linearRampToValueAtTime(0, audioCtx.currentTime + duration);
-
-   osc.frequency.setValueAtTime(frequency, audioCtx.currentTime);
-
-   osc.start();
-   osc.stop(audioCtx.currentTime + duration);
+function playSound(frequency, duration=defDuration){
+	const osc = audioCtx.createOscillator();
+	const envelope = audioCtx.createGain();
+	osc.connect(envelope);
+	envelope.connect(audioCtx.destination);
+	envelope.gain.setValueAtTime(0, audioCtx.currentTime);
+	envelope.gain.linearRampToValueAtTime(gainStep, audioCtx.currentTime+gainStep);
+	envelope.gain.linearRampToValueAtTime(0, audioCtx.currentTime+duration);
+	osc.frequency.setValueAtTime(frequency, audioCtx.currentTime);
+	osc.start();
+	osc.stop(audioCtx.currentTime + duration);
 }
